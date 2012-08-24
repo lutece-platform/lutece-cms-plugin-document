8de87196b6949fd4e2a5e985f4e7b6d19451402b
hohll <hohll@users.noreply.github.com>
2012-08-24 17:20:14 +0000

    LUTECE-1493  Bootstrap integration Update init_db_document.sqlto include icon from famfamfam family
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-document@43384 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/document/plugin/init_db_document.sql
+++ src/sql/plugins/document/plugin/init_db_document.sql
@@ -119,16 +119,14 @@ INSERT INTO document_space (id_space,id_parent,document_space_name,description,d
 --
 -- Dumping data for table document_space_action
 --
-INSERT INTO document_space_action (id_action,name_key,description_key,action_url,icon_url,action_permission) VALUES 
-(1,'document.spaces.action.createSpace.name','document.spaces.action.createSpace.description','jsp/admin/plugins/document/CreateSpace.jsp','images/admin/skin/plugins/document/actions/new_space.png','CREATE');
-INSERT INTO document_space_action (id_action,name_key,description_key,action_url,icon_url,action_permission) VALUES 
-(2,'document.spaces.action.deleteSpace.name','document.spaces.action.deleteSpace.description','jsp/admin/plugins/document/DeleteSpace.jsp','images/admin/skin/plugins/document/actions/delete.png','DELETE');
-INSERT INTO document_space_action (id_action,name_key,description_key,action_url,icon_url,action_permission) VALUES 
-(3,'document.spaces.action.modifySpace.name','document.spaces.action.modifySpace.description','jsp/admin/plugins/document/ModifySpace.jsp','images/admin/skin/plugins/document/actions/modify_space.png','MODIFY');
-INSERT INTO document_space_action (id_action,name_key,description_key,action_url,icon_url,action_permission) VALUES 
-(4,'document.spaces.action.manageUsers.name','document.spaces.action.manageUsers.description','jsp/admin/plugins/document/ManageSpaceUsers.jsp','images/admin/skin/plugins/document/actions/users.png','USERS');
-INSERT INTO document_space_action (id_action,name_key,description_key,action_url,icon_url,action_permission) VALUES 
-(5,'document.spaces.action.moveSpace.name','document.spaces.action.moveSpace.description','jsp/admin/plugins/document/MoveSpace.jsp','images/admin/skin/plugins/document/actions/move_space.png','MOVE');
+--
+-- Table document_space_action
+--
+INSERT INTO document_space_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (1, 'document.spaces.action.createSpace.name', 'document.spaces.action.createSpace.description', 'jsp/admin/plugins/document/CreateSpace.jsp', 'fam-folder-bell', 'CREATE');
+INSERT INTO document_space_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (2, 'document.spaces.action.deleteSpace.name', 'document.spaces.action.deleteSpace.description', 'jsp/admin/plugins/document/DeleteSpace.jsp', 'fam-folder-edit', 'DELETE');
+INSERT INTO document_space_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (3, 'document.spaces.action.modifySpace.name', 'document.spaces.action.modifySpace.description', 'jsp/admin/plugins/document/ModifySpace.jsp', 'fam-folder-error', 'MODIFY');
+INSERT INTO document_space_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (4, 'document.spaces.action.manageUsers.name', 'document.spaces.action.manageUsers.description', 'jsp/admin/plugins/document/ManageSpaceUsers.jsp', 'fam-folder-wrench', 'USERS');
+INSERT INTO document_space_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (5, 'document.spaces.action.moveSpace.name', 'document.spaces.action.moveSpace.description', 'jsp/admin/plugins/document/MoveSpace.jsp', 'fam-folder-heart', 'MOVE');
 
 --
 -- Dumping data for table document_space_document_type
@@ -300,42 +298,28 @@ INSERT INTO document_view (code_view,name_key) VALUES ('thumbnail','document.vie
 --
 -- Dumping data for table document_workflow_action
 --
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(1,'document.workflow.action.deleteDocument.name','document.workflow.action.deleteDocument.description','jsp/admin/plugins/document/DeleteDocument.jsp?','images/admin/skin/plugins/document/actions/delete.png','DELETE',null);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(2,'document.workflow.action.modifyDocument.name','document.workflow.action.modifyDocument.description','jsp/admin/plugins/document/ModifyDocument.jsp?','images/admin/skin/plugins/document/actions/modify.png','MODIFY',null);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(3,'document.workflow.action.submitForApproval.name','document.workflow.action.submitForApproval.description','jsp/admin/plugins/document/DoChangeState.jsp?id_state=2&amp;','images/admin/skin/plugins/document/actions/submit.png','SUBMIT',2);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(4,'document.workflow.action.approveDocument.name','document.workflow.action.approveDocument.description','jsp/admin/plugins/document/DoValidateDocument.jsp?id_state=3&amp;','images/admin/skin/plugins/document/actions/validate.png','VALIDATE',3);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(5,'document.workflow.action.assignDocument.name','document.workflow.action.assignDocument.description','jsp/admin/plugins/document/ManageDocumentPublishing.jsp?','images/admin/skin/plugins/document/actions/assign.png','ASSIGN',null);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(6,'document.workflow.action.rejectDocument.name','document.workflow.action.rejectDocument.description','jsp/admin/plugins/document/DoChangeState.jsp?id_state=4&amp;','images/admin/skin/plugins/document/actions/reject.png','VALIDATE',4);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(7,'document.workflow.action.submitAgain.name','document.workflow.action.submitAgain.description','jsp/admin/plugins/document/DoChangeState.jsp?id_state=2&amp;','images/admin/skin/plugins/document/actions/submit_again.png','SUBMIT',2);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(8,'document.workflow.action.archiveDocument.name','document.workflow.action.archiveDocument.description','jsp/admin/plugins/document/DoConfirmArchiveDocument.jsp?id_state=5&amp;','images/admin/skin/plugins/document/actions/archive.png','ARCHIVE',5);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(9,'document.workflow.action.changeDocument.name','document.workflow.action.changeDocument.description','jsp/admin/plugins/document/ModifyDocument.jsp?id_state=6&amp;','images/admin/skin/plugins/document/actions/change.png','CHANGE',6);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(10,'document.workflow.action.submitChanges.name','document.workflow.action.submitChanges.description','jsp/admin/plugins/document/DoChangeState.jsp?id_state=7&amp;','images/admin/skin/plugins/document/actions/submit_changes.png','SUBMIT',7);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES  
-(11,'document.workflow.action.rejectChanges.name','document.workflow.action.rejectChanges.description','jsp/admin/plugins/document/DoChangeState.jsp?id_state=6&amp;','images/admin/skin/plugins/document/actions/reject_changes.png','VALIDATE',6);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(12,'document.workflow.action.approveChanges.name','document.workflow.action.approveChanges.description','jsp/admin/plugins/document/DoValidateDocument.jsp?id_state=3&amp;','images/admin/skin/plugins/document/actions/validate.png','VALIDATE',3);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(13,'document.workflow.action.unarchiveDocument.name','document.workflow.action.unarchiveDocument.description','jsp/admin/plugins/document/DoChangeState.jsp?id_state=3&amp;','images/admin/skin/plugins/document/actions/unarchive.png','ARCHIVE',3);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(14,'document.workflow.action.history.name','document.workflow.action.history.description','jsp/admin/plugins/document/DocumentHistory.jsp?','images/admin/skin/plugins/document/actions/history.png','VIEW_HISTORY',null);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(15,'document.workflow.action.previewDocument.name','document.workflow.action.previewDocument.description','jsp/admin/plugins/document/PreviewDocument.jsp?','images/admin/skin/plugins/document/actions/view.png','VIEW',null);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(16,'document.workflow.action.moveDocument.name','document.workflow.action.moveDocument.description','jsp/admin/plugins/document/MoveDocument.jsp?','images/admin/skin/plugins/document/actions/move.png','MOVE',null);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(17,'document.workflow.action.commentDocument.name','document.workflow.action.commentDocument.description','jsp/admin/plugins/document/modules/comment/ManageDocumentComments.jsp?','images/admin/skin/plugins/document/actions/comment.png','COMMENT',null);
-INSERT INTO document_workflow_action (id_action,name_key,description_key,action_url,icon_url,action_permission,id_finish_state) VALUES 
-(18,'document.workflow.action.publishDocument.name','document.workflow.action.publishDocument.description',NULL,NULL,'PUBLISH',null);
+--
+-- Table document_workflow_action
+--
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (1, 'document.workflow.action.deleteDocument.name', 'document.workflow.action.deleteDocument.description', 'jsp/admin/plugins/document/DeleteDocument.jsp?', 'fam-delete', 'DELETE', NULL);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (2, 'document.workflow.action.modifyDocument.name', 'document.workflow.action.modifyDocument.description', 'jsp/admin/plugins/document/ModifyDocument.jsp?', 'fam-page-white-excel', 'MODIFY', NULL);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (3, 'document.workflow.action.submitForApproval.name', 'document.workflow.action.submitForApproval.description', 'jsp/admin/plugins/document/DoChangeState.jsp?id_state=2&amp;', 'fam-page-white-magnify', 'SUBMIT', 2);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (4, 'document.workflow.action.approveDocument.name', 'document.workflow.action.approveDocument.description', 'jsp/admin/plugins/document/DoValidateDocument.jsp?id_state=3&amp;', 'fam-accept', 'VALIDATE', 3);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (5, 'document.workflow.action.assignDocument.name', 'document.workflow.action.assignDocument.description', 'jsp/admin/plugins/document/ManageDocumentPublishing.jsp?', 'fam-page-white-zip', 'ASSIGN', NULL);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (6, 'document.workflow.action.rejectDocument.name', 'document.workflow.action.rejectDocument.description', 'jsp/admin/plugins/document/DoChangeState.jsp?id_state=4&amp;', 'fam-stop', 'VALIDATE', 4);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (7, 'document.workflow.action.submitAgain.name', 'document.workflow.action.submitAgain.description', 'jsp/admin/plugins/document/DoChangeState.jsp?id_state=2&amp;', 'fam-page-white-magnify', 'SUBMIT', 2);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (8, 'document.workflow.action.archiveDocument.name', 'document.workflow.action.archiveDocument.description', 'jsp/admin/plugins/document/DoConfirmArchiveDocument.jsp?id_state=5&amp;', 'fam-page-white-edit', 'ARCHIVE', 5);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (9, 'document.workflow.action.changeDocument.name', 'document.workflow.action.changeDocument.description', 'jsp/admin/plugins/document/ModifyDocument.jsp?id_state=6&amp;', 'fam-page-white-excel', 'CHANGE', 6);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (10, 'document.workflow.action.submitChanges.name', 'document.workflow.action.submitChanges.description', 'jsp/admin/plugins/document/DoChangeState.jsp?id_state=7&amp;', 'fam-page-white-magnify', 'SUBMIT', 7);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (11, 'document.workflow.action.rejectChanges.name', 'document.workflow.action.rejectChanges.description', 'jsp/admin/plugins/document/DoChangeState.jsp?id_state=6&amp;', 'fam-stop', 'VALIDATE', 6);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (12, 'document.workflow.action.approveChanges.name', 'document.workflow.action.approveChanges.description', 'jsp/admin/plugins/document/DoValidateDocument.jsp?id_state=3&amp;', 'fam-accept', 'VALIDATE', 3);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (13, 'document.workflow.action.unarchiveDocument.name', 'document.workflow.action.unarchiveDocument.description', 'jsp/admin/plugins/document/DoChangeState.jsp?id_state=3&amp;', 'fam-package-go', 'ARCHIVE', 3);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (14, 'document.workflow.action.history.name', 'document.workflow.action.history.description', 'jsp/admin/plugins/document/DocumentHistory.jsp?', 'fam-book-previous', 'VIEW_HISTORY', NULL);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (15, 'document.workflow.action.previewDocument.name', 'document.workflow.action.previewDocument.description', 'jsp/admin/plugins/document/PreviewDocument.jsp?', 'fam-eye', 'VIEW', NULL);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (16, 'document.workflow.action.moveDocument.name', 'document.workflow.action.moveDocument.description', 'jsp/admin/plugins/document/MoveDocument.jsp?', 'fam-folder-heart', 'MOVE', NULL);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (17, 'document.workflow.action.commentDocument.name', 'document.workflow.action.commentDocument.description', 'jsp/admin/plugins/document/modules/comment/ManageDocumentComments.jsp?', 'fam-comment-add', 'COMMENT', NULL);
+INSERT INTO document_workflow_action (id_action, name_key, description_key, action_url, icon_url, action_permission, id_finish_state) VALUES (18, 'document.workflow.action.publishDocument.name', 'document.workflow.action.publishDocument.description', NULL, NULL, 'PUBLISH', NULL);
+
 
 --
 -- Dumping data for table document_workflow_state
