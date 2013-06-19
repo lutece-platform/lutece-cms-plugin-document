1bfe366ccfc877eabf3abb804d7c18cd2fa75c54
vbroussard <vbroussard@users.noreply.github.com>
2013-06-19 09:40:23 +0000

    DOCUMENT-209 : Correction of sonar issues
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-document@48949 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/document/business/workflow/IDocumentStateDAO.java
+++ src/java/fr/paris/lutece/plugins/document/business/workflow/IDocumentStateDAO.java
@@ -9,7 +9,7 @@ import java.util.Locale;
 
 
 /**
- * This class porvides Data Access methods for DocumentStateDAO interface
+ * This class provides Data Access methods for DocumentStateDAO interface
  */
 public interface IDocumentStateDAO
 {
@@ -19,12 +19,12 @@ public interface IDocumentStateDAO
      * @return the instance of the Rule
      * @param nDocumentStateId
      */
-    abstract DocumentState load( int nDocumentStateId );
+    DocumentState load( int nDocumentStateId );
 
     /**
      * Load the list of Document States
      * @return The Reference of the Document States
      * @param locale
      */
-    abstract ReferenceList selectDocumentStatesList( Locale locale );
+    ReferenceList selectDocumentStatesList( Locale locale );
 }
