d9b83f1406e419e1c6518a2e92d1498ac2a92dfe
RPU <RPU@bab10101-e421-0410-a517-8ce0973de3ef>
2014-03-05 09:02:03 +0000

    DOCUMENT-197 - An error of transformation XSLT appear when we create a space with a special character like & in the title of the space
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-document@64412 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/document/service/spaces/DocumentSpacesService.java
+++ src/java/fr/paris/lutece/plugins/document/service/spaces/DocumentSpacesService.java
@@ -59,6 +59,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 
 
@@ -93,10 +94,10 @@ public class DocumentSpacesService
 
     //CONSTANTS
     private static final String CONSTANT_TRUE = "true";
-    private static DocumentSpacesService _singleton = new DocumentSpacesService(  );
+    private static DocumentSpacesService _singleton = new DocumentSpacesService( );
 
     /** Creates a new instance of DocumentSpacesService */
-    private DocumentSpacesService(  )
+    private DocumentSpacesService( )
     {
     }
 
@@ -104,7 +105,7 @@ public class DocumentSpacesService
      * Returns the unique instance of the service
      * @return the unique instance of the service
      */
-    public static DocumentSpacesService getInstance(  )
+    public static DocumentSpacesService getInstance( )
     {
         return _singleton;
     }
@@ -116,38 +117,39 @@ public class DocumentSpacesService
      */
     public String getXmlSpacesList( AdminUser user )
     {
-        StringBuffer sbXML = new StringBuffer(  );
+        StringBuffer sbXML = new StringBuffer( );
         XmlUtil.beginElement( sbXML, TAG_SPACES );
 
         for ( DocumentSpace space : getUserSpaces( user ) )
         {
-            findSpaces( sbXML, space.getId(  ), user );
+            findSpaces( sbXML, space.getId( ), user );
         }
 
         XmlUtil.endElement( sbXML, TAG_SPACES );
 
-        return sbXML.toString(  );
+        return sbXML.toString( );
     }
 
     /**
-     * Gets allowed Spaces for a given user and a type of document as an XML document
+     * Gets allowed Spaces for a given user and a type of document as an XML
+     * document
      * @param user The current user
      * @param strCodeType The code for the document type
      * @return An XML document containing allowed spaces
      */
     public String getXmlSpacesList( AdminUser user, String strCodeType )
     {
-        StringBuffer sbXML = new StringBuffer(  );
+        StringBuffer sbXML = new StringBuffer( );
         XmlUtil.beginElement( sbXML, TAG_SPACES );
 
         for ( DocumentSpace space : getUserSpaces( user ) )
         {
-            findSpacesByCodeType( sbXML, space.getId(  ), user, strCodeType );
+            findSpacesByCodeType( sbXML, space.getId( ), user, strCodeType );
         }
 
         XmlUtil.endElement( sbXML, TAG_SPACES );
 
-        return sbXML.toString(  );
+        return sbXML.toString( );
     }
 
     /**
@@ -161,7 +163,7 @@ public class DocumentSpacesService
 
         for ( DocumentSpace space : getUserSpaces( user ) )
         {
-            nIdDefaultSpace = space.getId(  );
+            nIdDefaultSpace = space.getId( );
         }
 
         return nIdDefaultSpace;
@@ -169,7 +171,7 @@ public class DocumentSpacesService
 
     private Collection<DocumentSpace> getUserSpaces( AdminUser user )
     {
-        Collection<DocumentSpace> listSpaces = DocumentSpaceHome.findAll(  );
+        Collection<DocumentSpace> listSpaces = DocumentSpaceHome.findAll( );
         listSpaces = RBACService.getAuthorizedCollection( listSpaces, SpaceResourceIdService.PERMISSION_VIEW, user );
 
         return listSpaces;
@@ -179,7 +181,7 @@ public class DocumentSpacesService
      * Gets the XSL to display user spaces tree
      * @return The XSL to display user spaces tree
      */
-    public Source getTreeXsl(  )
+    public Source getTreeXsl( )
     {
         FileInputStream fis = AppPathService.getResourceAsStream( PATH_XSL, FILE_TREE_XSL );
         Source xslSource = new StreamSource( fis );
@@ -189,8 +191,9 @@ public class DocumentSpacesService
 
     /**
      * Build recursively the XML document containing the arborescence of spaces
-     *
-     * @param sbXML The buffer in which adding the current space of the arborescence
+     * 
+     * @param sbXML The buffer in which adding the current space of the
+     *            arborescence
      * @param nSpaceId The current space of the recursive course
      * @param user AdminUser
      */
@@ -201,20 +204,20 @@ public class DocumentSpacesService
         if ( AdminWorkgroupService.isAuthorized( space, user ) )
         {
             XmlUtil.beginElement( sbXML, TAG_SPACE );
-            XmlUtil.addElement( sbXML, TAG_SPACE_ID, space.getId(  ) );
-            XmlUtil.addElement( sbXML, TAG_SPACE_NAME, space.getName(  ) );
-            XmlUtil.addElement( sbXML, TAG_SPACE_DESCRIPTION, space.getDescription(  ) );
-            XmlUtil.addElement( sbXML, TAG_SPACE_ICON_URL, space.getIconUrl(  ) );
+            XmlUtil.addElement( sbXML, TAG_SPACE_ID, space.getId( ) );
+            XmlUtil.addElement( sbXML, TAG_SPACE_NAME, StringEscapeUtils.escapeXml( space.getName( ) ) );
+            XmlUtil.addElement( sbXML, TAG_SPACE_DESCRIPTION, space.getDescription( ) );
+            XmlUtil.addElement( sbXML, TAG_SPACE_ICON_URL, space.getIconUrl( ) );
 
             List<DocumentSpace> listChilds = DocumentSpaceHome.findChilds( nSpaceId );
 
-            if ( listChilds.size(  ) > 0 )
+            if ( listChilds.size( ) > 0 )
             {
                 XmlUtil.beginElement( sbXML, TAG_SPACE_CHILDS );
 
                 for ( DocumentSpace child : listChilds )
                 {
-                    findSpaces( sbXML, child.getId(  ), user );
+                    findSpaces( sbXML, child.getId( ), user );
                 }
 
                 XmlUtil.endElement( sbXML, TAG_SPACE_CHILDS );
@@ -247,7 +250,7 @@ public class DocumentSpacesService
 
             boolean bContainCodeType = false;
 
-            for ( String strCodeTypeDocument : space.getAllowedDocumentTypes(  ) )
+            for ( String strCodeTypeDocument : space.getAllowedDocumentTypes( ) )
             {
                 if ( strCodeTypeDocument.equals( strCodeType ) )
                 {
@@ -255,13 +258,13 @@ public class DocumentSpacesService
                 }
             }
 
-            if ( listChilds.size(  ) > 0 )
+            if ( listChilds.size( ) > 0 )
             {
-                StringBuffer sbChildrenXML = new StringBuffer(  );
+                StringBuffer sbChildrenXML = new StringBuffer( );
 
                 for ( DocumentSpace child : listChilds )
                 {
-                    bChildValidity = findSpacesByCodeType( sbChildrenXML, child.getId(  ), user, strCodeType );
+                    bChildValidity = findSpacesByCodeType( sbChildrenXML, child.getId( ), user, strCodeType );
 
                     if ( bChildValidity )
                     {
@@ -274,19 +277,20 @@ public class DocumentSpacesService
                 {
                     //the space has children which are valid or have valid children
                     XmlUtil.beginElement( sbXML, TAG_SPACE );
-                    XmlUtil.addElement( sbXML, TAG_SPACE_ID, space.getId(  ) );
+                    XmlUtil.addElement( sbXML, TAG_SPACE_ID, space.getId( ) );
 
-                    if ( bContainCodeType && space.isDocumentCreationAllowed(  ) &&
-                            ( RBACService.isAuthorized( documentType, DocumentTypeResourceIdService.PERMISSION_CREATE,
-                                user ) ) )
+                    if ( bContainCodeType
+                            && space.isDocumentCreationAllowed( )
+                            && ( RBACService.isAuthorized( documentType,
+                                    DocumentTypeResourceIdService.PERMISSION_CREATE, user ) ) )
                     {
                         //the user can create the selected type of document into the space
                         XmlUtil.addElement( sbXML, TAG_SPACE_IS_VALID, CONSTANT_TRUE );
                     }
 
-                    XmlUtil.addElement( sbXML, TAG_SPACE_NAME, space.getName(  ) );
-                    XmlUtil.addElement( sbXML, TAG_SPACE_DESCRIPTION, space.getDescription(  ) );
-                    XmlUtil.addElement( sbXML, TAG_SPACE_ICON_URL, space.getIconUrl(  ) );
+                    XmlUtil.addElement( sbXML, TAG_SPACE_NAME, space.getName( ) );
+                    XmlUtil.addElement( sbXML, TAG_SPACE_DESCRIPTION, space.getDescription( ) );
+                    XmlUtil.addElement( sbXML, TAG_SPACE_ICON_URL, space.getIconUrl( ) );
                     XmlUtil.beginElement( sbXML, TAG_SPACE_CHILDS );
                     sbXML.append( sbChildrenXML );
                     XmlUtil.endElement( sbXML, TAG_SPACE_CHILDS );
@@ -297,16 +301,17 @@ public class DocumentSpacesService
             //the space has no children
             else
             {
-                if ( ( bContainCodeType ) &&
-                        RBACService.isAuthorized( documentType, DocumentTypeResourceIdService.PERMISSION_CREATE, user ) )
+                if ( ( bContainCodeType )
+                        && RBACService.isAuthorized( documentType, DocumentTypeResourceIdService.PERMISSION_CREATE,
+                                user ) )
                 {
                     //the space can contain the type of document
                     XmlUtil.beginElement( sbXML, TAG_SPACE );
-                    XmlUtil.addElement( sbXML, TAG_SPACE_ID, space.getId(  ) );
+                    XmlUtil.addElement( sbXML, TAG_SPACE_ID, space.getId( ) );
                     XmlUtil.addElement( sbXML, TAG_SPACE_IS_VALID, CONSTANT_TRUE );
-                    XmlUtil.addElement( sbXML, TAG_SPACE_NAME, space.getName(  ) );
-                    XmlUtil.addElement( sbXML, TAG_SPACE_DESCRIPTION, space.getDescription(  ) );
-                    XmlUtil.addElement( sbXML, TAG_SPACE_ICON_URL, space.getIconUrl(  ) );
+                    XmlUtil.addElement( sbXML, TAG_SPACE_NAME, space.getName( ) );
+                    XmlUtil.addElement( sbXML, TAG_SPACE_DESCRIPTION, space.getDescription( ) );
+                    XmlUtil.addElement( sbXML, TAG_SPACE_ICON_URL, space.getIconUrl( ) );
                     XmlUtil.endElement( sbXML, TAG_SPACE );
                     bValidSpace = true;
                 }
@@ -325,7 +330,7 @@ public class DocumentSpacesService
      * Check if the user can view a space according its role
      * @param nIdSpace The Space Id
      * @param user The current user
-     * @return True if the user has the permission to view document   Space.
+     * @return True if the user has the permission to view document Space.
      */
     public boolean isAuthorizedViewByRole( int nIdSpace, AdminUser user )
     {
@@ -341,7 +346,7 @@ public class DocumentSpacesService
                 return true;
             }
 
-            space = DocumentSpaceHome.findByPrimaryKey( space.getIdParent(  ) );
+            space = DocumentSpaceHome.findByPrimaryKey( space.getIdParent( ) );
         }
 
         return false;
@@ -361,7 +366,7 @@ public class DocumentSpacesService
 
             if ( ( space != null ) && AdminWorkgroupService.isAuthorized( space, user ) )
             {
-                return isAuthorizedViewByWorkgroup( space.getIdParent(  ), user );
+                return isAuthorizedViewByWorkgroup( space.getIdParent( ), user );
             }
 
             return false;
@@ -377,8 +382,8 @@ public class DocumentSpacesService
      */
     public List<DocumentSpace> getUserAllowedSpaces( AdminUser user )
     {
-        List<DocumentSpace> listSpaces = new ArrayList<DocumentSpace>(  );
-        List<DocumentSpace> listSpacesAllowed = new ArrayList<DocumentSpace>(  );
+        List<DocumentSpace> listSpaces = new ArrayList<DocumentSpace>( );
+        List<DocumentSpace> listSpacesAllowed = new ArrayList<DocumentSpace>( );
 
         for ( DocumentSpace space : getUserSpaces( user ) )
         {
@@ -388,7 +393,7 @@ public class DocumentSpacesService
         //verify authorization workgroup
         for ( DocumentSpace spaceAllowed : listSpaces )
         {
-            if ( isAuthorizedViewByWorkgroup( spaceAllowed.getId(  ), user ) )
+            if ( isAuthorizedViewByWorkgroup( spaceAllowed.getId( ), user ) )
             {
                 listSpacesAllowed.add( spaceAllowed );
             }
@@ -399,20 +404,22 @@ public class DocumentSpacesService
 
     /**
      * get the HTML code to display a space browser.
-     *
+     * 
      * @param request The HTTP request
      * @param user The current user
-     * @param bFilterViewRollUser true if the list of childs space must be filter by RBAC view permission
-     * @param bFilterWorkspaceUser true if the list of childs space must be filter by workgroup
+     * @param bFilterViewRollUser true if the list of childs space must be
+     *            filter by RBAC view permission
+     * @param bFilterWorkspaceUser true if the list of childs space must be
+     *            filter by workgroup
      * @param locale The Locale
      * @return The HTML form
      */
     public String getSpacesBrowser( HttpServletRequest request, AdminUser user, Locale locale,
-        boolean bFilterViewRollUser, boolean bFilterWorkspaceUser )
+            boolean bFilterViewRollUser, boolean bFilterWorkspaceUser )
     {
         String strIdSpaceFilter = request.getParameter( PARAMETER_BROWSER_SELECTED_SPACE_ID );
         String strIdSpace = request.getParameter( PARAMETER_BROWSER_SPACE_ID );
-        Map<String, Object> model = new HashMap<String, Object>(  );
+        Map<String, Object> model = new HashMap<String, Object>( );
         DocumentSpace selectedSpace = null;
         DocumentSpace space;
         Collection<DocumentSpace> spacesList;
@@ -430,7 +437,7 @@ public class DocumentSpacesService
         // if current space doesn't exists then set it up
         if ( IntegerUtils.isNotNumeric( strIdSpace ) )
         {
-            nIdSpace = DocumentSpacesService.getInstance(  ).getUserDefaultSpace( user );
+            nIdSpace = DocumentSpacesService.getInstance( ).getUserDefaultSpace( user );
         }
         else
         {
@@ -440,7 +447,7 @@ public class DocumentSpacesService
         // set space list
         if ( nIdSpace == -1 )
         {
-            space = new DocumentSpace(  );
+            space = new DocumentSpace( );
             space.setId( -1 );
             space.setIdParent( -1 );
         }
@@ -449,11 +456,11 @@ public class DocumentSpacesService
             space = DocumentSpaceHome.findByPrimaryKey( nIdSpace );
         }
 
-        spacesList = DocumentSpaceHome.findChilds( space.getId(  ) );
+        spacesList = DocumentSpaceHome.findChilds( space.getId( ) );
 
         if ( bFilterViewRollUser )
         {
-            bIsAuthorized = isAuthorizedViewByRole( space.getId(  ), user );
+            bIsAuthorized = isAuthorizedViewByRole( space.getId( ), user );
         }
 
         if ( bIsAuthorized && bFilterWorkspaceUser )
@@ -467,17 +474,17 @@ public class DocumentSpacesService
         }
 
         // set links for childs spaces
-        int[] arrayHasChilds = new int[spacesList.size(  )];
+        int[] arrayHasChilds = new int[spacesList.size( )];
 
         for ( DocumentSpace documentSpace : spacesList )
         {
             // Check Childs spaces
-            List<DocumentSpace> childsSpaces = DocumentSpaceHome.findChilds( documentSpace.getId(  ) );
+            List<DocumentSpace> childsSpaces = DocumentSpaceHome.findChilds( documentSpace.getId( ) );
 
             //If childs spaces list is not empty, then add into array
-            if ( childsSpaces.size(  ) != 0 )
+            if ( childsSpaces.size( ) != 0 )
             {
-                arrayHasChilds[i] = documentSpace.getId(  );
+                arrayHasChilds[i] = documentSpace.getId( );
             }
 
             i++;
@@ -494,18 +501,18 @@ public class DocumentSpacesService
         model.put( MARK_SPACE, space );
         model.put( MARK_SPACES_LIST, spacesList );
         model.put( MARK_URLS_LIST, arrayHasChilds );
-        model.put( MARK_ACTION, request.getRequestURI(  ) );
+        model.put( MARK_ACTION, request.getRequestURI( ) );
 
         HtmlTemplate template = AppTemplateService.getTemplate( TEMPLATE_BROWSE_SPACES, locale, model );
 
-        return template.getHtml(  );
+        return template.getHtml( );
     }
 
     private void addChildSpaces( DocumentSpace spaceParent, List<DocumentSpace> listSpaces )
     {
         listSpaces.add( spaceParent );
 
-        List<DocumentSpace> listChilds = DocumentSpaceHome.findChilds( spaceParent.getId(  ) );
+        List<DocumentSpace> listChilds = DocumentSpaceHome.findChilds( spaceParent.getId( ) );
 
         for ( DocumentSpace space : listChilds )
         {
@@ -514,23 +521,26 @@ public class DocumentSpacesService
     }
 
     /**
-     * the list of parents Document space of the document space specified in parameter
+     * the list of parents Document space of the document space specified in
+     * parameter
      * @param documentSpace the document space
      * @param user the user
-     * @return the list of parents Document space of the document space specified in parameter
+     * @return the list of parents Document space of the document space
+     *         specified in parameter
      */
     private List<DocumentSpace> getSpaceParents( DocumentSpace documentSpace, AdminUser user )
     {
-        List<DocumentSpace> documentSpaceParents = new ArrayList<DocumentSpace>(  );
-        getSpaceParents( documentSpace.getIdParent(  ), documentSpaceParents, user );
+        List<DocumentSpace> documentSpaceParents = new ArrayList<DocumentSpace>( );
+        getSpaceParents( documentSpace.getIdParent( ), documentSpaceParents, user );
 
         return documentSpaceParents;
     }
 
     /**
-     * add in the list of document space the list of parents document space specified in parameter
+     * add in the list of document space the list of parents document space
+     * specified in parameter
      * @param nSpaceId the id of the document space
-     * @param documentSpaceParents the  list of document space
+     * @param documentSpaceParents the list of document space
      * @param user the user
      */
     private void getSpaceParents( int nSpaceId, List<DocumentSpace> documentSpaceParents, AdminUser user )
@@ -539,9 +549,9 @@ public class DocumentSpacesService
 
         if ( AdminWorkgroupService.isAuthorized( documentSpace, user ) )
         {
-            if ( documentSpace.getIdParent(  ) != -1 )
+            if ( documentSpace.getIdParent( ) != -1 )
             {
-                getSpaceParents( documentSpace.getIdParent(  ), documentSpaceParents, user );
+                getSpaceParents( documentSpace.getIdParent( ), documentSpaceParents, user );
             }
 
             documentSpaceParents.add( documentSpace );
@@ -557,24 +567,24 @@ public class DocumentSpacesService
     public String getLabelSpacePath( int nIdDocumentSpace, AdminUser user )
     {
         DocumentSpace documentSpace = DocumentSpaceHome.findByPrimaryKey( nIdDocumentSpace );
-        StringBuffer sbLabelPath = new StringBuffer(  );
+        StringBuffer sbLabelPath = new StringBuffer( );
 
         if ( documentSpace != null )
         {
-            if ( documentSpace.getIdParent(  ) != -1 )
+            if ( documentSpace.getIdParent( ) != -1 )
             {
                 List<DocumentSpace> documentSpaceParents = getSpaceParents( documentSpace, user );
 
                 for ( DocumentSpace documentSpaceParent : documentSpaceParents )
                 {
-                    sbLabelPath.append( documentSpaceParent.getName(  ) );
+                    sbLabelPath.append( documentSpaceParent.getName( ) );
                     sbLabelPath.append( "/" );
                 }
             }
 
-            sbLabelPath.append( documentSpace.getName(  ) );
+            sbLabelPath.append( documentSpace.getName( ) );
         }
 
-        return sbLabelPath.toString(  );
+        return sbLabelPath.toString( );
     }
 }
