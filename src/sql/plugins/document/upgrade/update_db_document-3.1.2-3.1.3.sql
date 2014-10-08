d9090a6da9d9b4a569448ee11dd9e7a8d9a78eda
hohll <hohll@users.noreply.github.com>
2014-10-08 12:03:28 +0000

    DOCUMENT-254 Update to glyphicon icon library
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-document@71550 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/document/upgrade/update_db_document-3.1.2-3.1.3.sql
+++ src/sql/plugins/document/upgrade/update_db_document-3.1.2-3.1.3.sql
@@ -1,2 +1,6 @@
 INSERT INTO document_attr_type_parameter (code_attr_type, parameter_name, parameter_label_key, parameter_index, parameter_description_key, parameter_default_value) VALUES ('file', 'extension', 'document.attributeType.file.parameter.extension.label', 1, 'document.attributeType.file.parameter.extension.description', 'docx,xlsx,pdf');
 INSERT INTO document_attr_type_parameter (code_attr_type, parameter_name, parameter_label_key, parameter_index, parameter_description_key, parameter_default_value) VALUES ('image', 'extension', 'document.attributeType.image.parameter.extension.label', 1, 'document.attributeType.image.parameter.extension.description', 'jpg,jpeg,png');
+
+/* Update icons for Bootstrap 3 */
+UPDATE document_space_action SET icon_url=CONCAT('glyphicon glyph', icon_url);
+UPDATE document_workflow_action SET icon_url=CONCAT('glyphicon glyph', icon_url);
