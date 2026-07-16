b60cf37e59f4d7970d477dcb44291f9cee46999f
FrancoisEricMerlin <francoiseric.merlin@paris.fr>
2026-07-16 14:19:03 +0200

    LUT-33036 : remove jackson-mapper-asl 1.5.0 (migrate to Jackson 2.x)
    Fixes CVE-2017-7525 by dropping the unmaintained org.codehaus.jackson
    (Jackson 1.x) dependency and migrating the only usage in DefaultManager
    to com.fasterxml.jackson (Jackson 2.x, already provided transitively by
    lutece-core). The readTree/path API is identical.
    
    - DefaultManager: JsonNode / ObjectMapper -> com.fasterxml.jackson.databind
    - pom.xml: remove jackson-mapper-asl:1.5.0 (no new dependency added)

--- src/java/fr/paris/lutece/plugins/document/service/attributes/DefaultManager.java
+++ src/java/fr/paris/lutece/plugins/document/service/attributes/DefaultManager.java
@@ -49,8 +49,8 @@ import fr.paris.lutece.util.html.HtmlTemplate;
 
 import org.apache.commons.lang3.StringUtils;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
